4b99c9ea724be8d81f158962307accf53184db94
wrolanda <wrolanda@wrolandas-MacBook-Pro.local>
2021-11-07 15:01:34 +0300

    version 1

---
+++ ft_lstnew.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/05 09:57:00 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/11/05 13:23:07 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*Allocates (with malloc(3)) and returns a new
+element. The variable ’content’ is initialized
+with the value of the parameter ’content’. The
+variable ’next’ is initialized to NULL.*/
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new;
+
+	new = NULL;
+	new = malloc(sizeof(t_list));
+	if (new)
+	{
+		new->content = content;
+		new->next = NULL;
+	}
+	return (new);
+}
