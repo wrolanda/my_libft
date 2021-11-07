92888b6ecaef9447fc8c6b276f306f6da2834bf2
wrolanda <wrolanda@wrolandas-MacBook-Pro.local>
2021-11-07 16:05:45 +0300

    incomplit

---
+++ ft_striteri.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 12:16:05 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/10/25 12:51:43 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	int	k;
+
+	k = 0;
+	if (s == NULL || f == NULL)
+		return ;
+	while (k < (int)ft_strlen(s))
+	{
+		(*f)(k, s);
+		k++;
+		s++;
+	}
+}
+
+/*#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	int	k;
+
+	k = 0;
+	if (s == NULL || f == NULL)
+		return ;
+	while (k < (int)ft_strlen(s))
+	{
+		(*f)(k, s);
+		k++;
+	}
+}*/
+
+/*void	ft_iter(unsigned int i, char *s)
+{
+	s[i] += 1;
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	int i = 0;
+	void (*f)(unsigned int i, char *s) = ft_iter;
+	char *str = "abcde";
+	ft_striteri(str,(*f));
+	printf ("\nres = (%s)\n", str);
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+	return (0);
+}*/
