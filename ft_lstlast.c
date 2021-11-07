51aaa61dbccae2afecbee33e3c769f2fff06be3d
wrolanda <wrolanda@wrolandas-MacBook-Pro.local>
2021-11-07 16:26:22 +0300

    first version

---
+++ ft_lstlast.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/07 16:19:28 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/11/07 16:23:12 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*Returns the last element of the list.*/
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next != NULL)
+		lst = lst->next;
+	return (lst);
+}
