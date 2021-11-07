8c761453fb50c3218fdeb1c93f85fe38a42f3506
wrolanda <wrolanda@wrolandas-MacBook-Pro.local>
2021-11-07 16:06:50 +0300

    fd functions final version

---
+++ ft_putstr_fd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/25 12:46:28 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/10/25 13:06:29 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (i < ft_strlen(s))
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	int	i1 = 0;
+	//int	i2 = 0;
+	char	*res;
+	
+	while (i1++ < 10)
+	{
+		res = ft_itoa(i1);
+		write (1, &res[0], 1);
+	}
+}*/
