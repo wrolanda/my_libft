9e687b85393c4e6fbe6c4618f34bba16b330d1a8
wrolanda <92958734+wrolanda@users.noreply.github.com>
2021-11-07 18:04:43 +0300

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -24,3 +24,15 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	else
 		*lst = new;
 }
+/*void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	last_elem;
+
+	if (*lst)
+	{
+		last_elem = ft_lstlast(*lst);
+		last_elem->next = new;
+	}
+	else
+		*lst = new;
+}*/
