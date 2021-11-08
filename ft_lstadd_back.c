bc7e4aeb4e9b5b1e5cc11bc865514e50dd141ff9
wrolanda <wrolanda@wrolandas-MacBook-Pro.local>
2021-11-08 18:35:19 +0300

    pointer to begin of list updated

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 16:27:59 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/07 16:48:51 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/08 13:19:10 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,28 @@
 /*Adds the element ’new’ at the end of the list.*/
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*last_elem;
+
+	if (*lst)
+	{
+		last_elem = ft_lstlast(*lst);
+		last_elem->next = new;
+	}
+	else
+		*lst = new;
+}
+/*void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*first_elem;
+
+	first_elem = *lst;
 	if (*lst)
 	{
 		while ((*lst)->next != NULL)
 			(*lst) = (*lst)->next;
 		(*lst)->next = new;
+		*(lst) = first_elem;
 	}
 	else
 		*lst = new;
-}
+}*/
