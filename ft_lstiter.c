6e1bc0c56082b25ad8023484620700ddd1e1c927
wrolanda <wrolanda@wrolandas-MacBook-Pro.local>
2021-11-09 01:26:00 +0300

    final version

---
+++ ft_lstiter.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 22:14:25 by wrolanda          #+#    #+#             */
+/*   Updated: 2021/11/09 01:25:21 by wrolanda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+/*DESCRIPTION
+Iterates the list ’lst’ and applies the function
+’f’ to the content of each element.
+PARAMETERS
+#1. The adress of a pointer to an element.
+#2. The adress of the function used to iterate on
+the list.*/
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*temp;
+
+	temp = lst;
+	if (!lst || !(*f))
+		return ;
+	while (temp != NULL)
+	{
+		(*f)(temp->content);
+		temp = temp->next;
+	}
+}
