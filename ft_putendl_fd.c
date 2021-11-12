83aaa24ce91f01e70c05fd12d40d4facc2de9968
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:33:00 +0300

    final version

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,11 +6,10 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 13:09:50 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/10 16:57:31 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:40:33 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 /*
 DESCRIPTION 
 Outputs the string ’s’ to the given file
@@ -20,6 +19,8 @@ PARAMETERS
 #1. The string to output.
 #2. The file descriptor on which to write.
 */
+#include "libft.h"
+
 void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
