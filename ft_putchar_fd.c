39538ba6da79bcb197d3477cc1c10afdf497158f
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:32:52 +0300

    final version

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,11 +6,10 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 12:42:44 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/10 16:57:05 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:40:24 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 /*
 DESCRIPTION 
 Outputs the character ’c’ to the given file
@@ -20,6 +19,8 @@ PARAMETERS
 #1. The character to output.
 #2. The file descriptor on which to write.
 */
+#include "libft.h"
+
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
