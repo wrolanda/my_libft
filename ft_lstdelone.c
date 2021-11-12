e9939b4a85e01d003a3baebb98422c65350125dc
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:31:13 +0300

    final version

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,11 +6,9 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 17:03:39 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/08 19:38:47 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:37:45 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 /*DESCRIPTION
 Takes as a parameter an element and frees the
@@ -21,6 +19,7 @@ PARAMETERS
 #1. The element to free.
 #2. The address of the function used to delete the
 content.*/
+#include "libft.h"
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
