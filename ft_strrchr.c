c33cfb6b8496877f30c605573aacddb4233c5e39
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:35:37 +0300

    final version

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,13 +6,14 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 18:19:16 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/10/18 21:17:51 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:42:38 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*	The strrchr() function is identical to strchr(), except it locates the
+	last occurrence of c.*/
 #include "libft.h"
 
-/*strrchr – поиск последнего вхождения символа в строку*/
 char	*ft_strrchr(const char *s, int c)
 {
 	char		*last_s;
@@ -29,7 +30,6 @@ char	*ft_strrchr(const char *s, int c)
 	return ((char *)last_s);
 }
 
-/*strrchr – поиск последнего вхождения символа в строку*/
 /*char	*ft_strrchr(const char *s, int c)
 {
 	long int	i;
@@ -46,25 +46,19 @@ char	*ft_strrchr(const char *s, int c)
 	return ((char *) 0);
 }*/
 
-/*#include <stdio.h>  // Для printf
+/*#include <stdio.h>
 int main (void)
 {
-   // Массив со строкой для поиска
    char str [11] = "123456780\0";
-   // Код искомого символа
    int ch = '\0';
-   // Указатель на искомую переменную в строке,
-   // по которой осуществляется поиск.
    char *ach;
   
-   // Ищем символ ‘6’
    ach=ft_strrchr(str, ch);
 
-   // Выводим результат на консоль
    if (ach==NULL)
-      printf ("Символ в строке не найден\n");
+      printf ("symbol not found\n");
    else
-      printf ("Искомый символ есть");
+      printf ("symbol found");
 
    return 0;
 }*/
