290bce63916a240360ff1381da89e3dd8777562b
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:31:39 +0300

    final version

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,16 +6,16 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 09:57:00 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/10 16:59:11 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/11 17:08:15 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "libft.h"
 
 /*Allocates (with malloc(3)) and returns a new
 element. The variable ’content’ is initialized
 with the value of the parameter ’content’. The
 variable ’next’ is initialized to NULL.*/
+#include "libft.h"
+
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*new;
