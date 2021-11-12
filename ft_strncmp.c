e574c87c874e7bfbcb9b91f9677853f4a7813904
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:35:11 +0300

    final version

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,13 +6,20 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 20:05:02 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/10/13 19:01:23 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:42:19 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*strcmp, strncmp -- compare strings
+DESCRIPTION
+	The strcmp() and strncmp() functions lexicographically compare the null-
+	terminated strings s1 and s2.
+
+	The strncmp() function compares not more than n characters.  Because
+	strncmp() is designed for comparing strings rather than binary data,
+	characters that appear after a `\0' character are not compared.*/
 #include "libft.h"
 
-/*strncmp – сравнение строк с ограничением количества сравниваемых символов.*/
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
