8568cba50f4a1a570fd334624f4dc62d8092ceb9
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:33:05 +0300

    final version

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,11 +6,10 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 13:17:28 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/10 16:58:02 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/11 17:08:55 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 /*
 DESCRIPTION 
 Outputs the integer ’n’ to the given file
@@ -20,6 +19,8 @@ PARAMETERS
 #1. The integer to output.
 #2. The file descriptor on which to write.
 */
+#include "libft.h"
+
 void	ft_putnbr_fd(int n, int fd)
 {	
 	if (n < 0)
