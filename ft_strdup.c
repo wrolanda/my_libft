4f1cb2c889c6aadfaa909fb9e5d7042154860038
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:33:54 +0300

    final version

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,13 +6,17 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 17:32:33 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/10/19 18:03:25 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:41:20 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*strdup -- save a copy of a string
+DESCRIPTION
+	The strdup() function allocates sufficient memory for a copy of the string s1,
+	does the copy, and returns a pointer to it.  The pointer may subsequently be
+	used as an argument to the function free(3).*/
 #include "libft.h"
 
-/*strdup – дублирование строк с выделением памяти под новую строку.*/
 char	*ft_strdup(const char *s1)
 {
 	char	*s2;
