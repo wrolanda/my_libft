faa6b9f68f4d839c1d48328dbe34a23cca848177
Whatever Rolanda <wrolanda@oa-b5.msk.21-school.ru>
2021-11-12 22:31:05 +0300

    final version

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,11 +6,10 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 19:40:59 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/08 22:11:43 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/10 20:37:36 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 /*DESCRIPRION
 Deletes and frees the given element and every
 successor of that element, using the function ’del’
@@ -20,6 +19,8 @@ PARAMETERS
 #1. The address of a pointer to an element.
 #2. The address of the function used to delete the
 content of the element.*/
+#include "libft.h"
+
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*temp;
